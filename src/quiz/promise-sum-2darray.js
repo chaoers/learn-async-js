24527cfb3ec12c793659b7246d886cd7e2b522b8
Bryant-surface <chaoers@bu.edu>
2024-02-28 16:27:25 -0500

    class activity

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,3 +1,18 @@
+function sumOfRow(arr, row) {
+    return new Promise((resolve, reject) => {
+        if (arr.length > row) {
+            setTimeout( () => {
+                let sum = 0;
+                for (let i = 0; i < arr[row].length; i++) {
+                    sum += arr[row][i];
+                }
+                resolve(sum);
+            }, 0);
+        }
+
+    })
+}
+
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
